18ecd03324d7c833925a1d00497a984cec0c922d
cg33 <chg80333@163.com>
2020-05-04 10:49:34 +0800

    add element theme

--- frontend/src/element/src/permission.js
+++ frontend/src/element/src/permission.js
@@ -19,6 +19,7 @@ router.beforeEach(async(to, from, next) => {
 
   // determine whether the user has logged in
   const hasToken = getToken()
+  // const hasToken = true
 
   if (hasToken) {
     if (to.path === '/login') {
@@ -35,6 +36,8 @@ router.beforeEach(async(to, from, next) => {
           // get user info
           // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
           const { roles } = await store.dispatch('user/getInfo')
+
+          // const roles = ['admin']
 
           // generate accessible routes map based on roles
           const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
